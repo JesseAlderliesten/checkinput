74610088394813b752e0fba15a23f94093612b17
JesseAlderliesten <jessealderliesten@hotmail.com>
2025-12-10 19:04:11 +0100

    Also test behaviour of data.frames, matrices, and lists.

--- inst/tinytest/test_is_number.R
+++ inst/tinytest/test_is_number.R
@@ -13,6 +13,7 @@ for(x in list(-Inf, -314, 0, 314, Inf, NaN)) {
 }
 
 for(x in list(NULL, FALSE, TRUE, NA, numeric(0), c(NA_real_, NA_real_),
-              c(NaN, NaN), c(314, 314), character(0), NA_character_, "", "nco")) {
+              c(NaN, NaN), c(314, 314), character(0), NA_character_, "", "nco",
+              data.frame(a = 314), matrix(314), list(314))) {
   expect_false(is_number(x = x))
 }
