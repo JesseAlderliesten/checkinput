7751bb7117468be5dbcbbf9c0fc3005280d55d5e
JesseAlderliesten <jessealderliesten@hotmail.com>
2025-12-12 16:22:58 +0100

    Documentation: add a description. In the 'See also' section, refer to the vignette and use @family to get cross-references. Add examples for is_nonnegative(), all_nonnegative(), is_positive(), and all_positive().

--- R/is_number.R
+++ R/is_number.R
@@ -1,16 +1,21 @@
-#' Check if x is a numeric vector of the correct length with numbers of the
+#' Check if `x` is numeric
+#'
+#' Checks if `x` is a numeric vector of the correct length with numbers of the
 #' correct sign.
 #'
 #' @inheritParams is_character
 #'
-#' @returns A single non-NA logical value indicating if `x` is a numeric vector
-#' of the correct length with numbers of the correct sign.
+#' @returns `TRUE` or `FALSE` indicating if `x` is a numeric vector of the
+#' correct length with numbers of the correct sign.
 #'
 #' @export
 #'
-#' @note `NA_real_` and `NA_integer_` is numeric, whereas `NA_complex_` is not.
+#' @note `NA_real_` and `NA_integer_` are numeric, whereas `NA_complex_` is not.
 #'
-#' @seealso [is_logical()] [is_character()]
+#' @seealso The vignette about type conversion:
+#' `vignette("Type_Coercion", package = "checkinput")`.
+#'
+#' @family collections of checks on type and length
 #'
 #' @examples
 #' is_number(1) # TRUE
@@ -19,6 +24,11 @@
 #' is_number("a") # FALSE: incorrect type
 #' is_number(NA_real_) # TRUE: `allow_NA` has not yet been implemented
 #' is_number(NA_character_) # FALSE: incorrect type
+#' is_nonnegative(3) # TRUE
+#' all_nonnegative(c(3, 0)) # TRUE
+#' is_positive(3) # TRUE
+#' is_positive(0) # FALSE
+#' all_positive(c(3, 0)) # FALSE
 is_number <- function(x) {
   is.numeric(x) && length(x) == 1L && is.null(dim(x))
 }
