1ba6b3b772358298eccc886de10370eec640547a
JesseAlderliesten <jessealderliesten@hotmail.com>
2025-12-21 15:38:53 +0100

    Add functions checking and modifying character vectors.

---
+++ R/all_names.R
@@ -0,0 +1,185 @@
+#' Check if `x` only contains syntactically valid names
+#'
+#' Check if `x` is a character vector that only contains unique, syntactically
+#' valid names that do not suggest they were automatically created or modified.
+#'
+#' @param x Vector of names to test.
+#' @param allow_dupl `TRUE` or `FALSE`: allow duplicate names?
+#' @param allow_susp `TRUE` or `FALSE`: allow suspicious names?
+#'
+#' @details
+#' [Syntactically valid names][base::make.names] are names that (1) only consist
+#' of letters, numbers, dots and underscores; (2) start with a letter or with a
+#' dot not followed by a number; (3) are not [reserved words][Reserved] such as
+#' [for] or any of the [NA]s.
+#'
+#' Duplicated names, which *are* syntactically valid, are *not* allowed if
+#' argument `allow_dupl` is `FALSE`. Suspicious names are not allowed if
+#' argument `allow_susp` is `FALSE`. This function distinguishes two kinds of
+#' suspicious names (see `Programming note 2` for the structure of the regular
+#' expressions used to identify them):
+#'
+#' - Names that might have been created by [utils::read.csv()] to name unnamed
+#'   columns, either because a particular column was unnamed or because data was
+#'   inadvertently present in a supposedly empty (and thus unnamed) column that
+#'   was read into \R: column names created by `read.csv()` have pattern `X`,
+#'   `X.1`, `X.2`, `...` if `header` is `TRUE` and pattern `V1`, `V2`, `V3`,
+#'   `...` if `header` is `FALSE`.
+#' - Names that might have been modified by
+#'   [make.names(..., unique = TRUE)][base::make.names()] to make duplicated
+#'   names unique: a dot and a number (starting at `1` for the first duplicate)
+#'   is added to duplicated names to make them unique. This is also used by
+#'   read.csv().
+#'
+#' Invalid names are *not* reported as suspicious names. In addition, duplicated
+#' names are reported only *once* in warnings emitted by `all_names()`.
+#'
+#' @returns `TRUE` or `FALSE` indicating if `x` is a character vector that only
+#' contains unique, syntactically valid names.
+#'
+#' @section Programming note: This implementation is based on the legacy-code of
+#' `has_colnames()` and `has_names()`, although significant changes have been
+#' made: `x` should be the names themselves, not an object to check the names
+#' of; `NA` is not allowed for arguments other than `x`; argument
+#' `allow_duplicated` was shortened to `allow_dupl`; arguments `check_syntax`
+#' and `silently` were removed.
+#'
+#' @section Programming note 2: The [regular expressions][base::regex] that are
+#' used to identify suspicious names contain the following elements: (1) require
+#' a pattern to start at the beginning of a string: `^`; (2) contain an `X` or a
+#' `V` followed by a literal dot: `X\\.` or `V\\.`; (3) contain one or more
+#' digits: `[[:digit:]]+`; (4) require a pattern to reach the end of the string:
+#' `$`.
+#'
+#' These elements are used to identify suspicious names as: (1) column names
+#' that start with an `X` followed by a dot and one or more digits until the end
+#' of the string: `^X\\.[[:digit:]]+$`; (2) column names that start with a `V`
+#' followed by one or more digits until the end of the string: `^V[[:digit:]]+$`;
+#' (3) column names that contain a dot followed by one or more digits until the
+#' end of the string: `\\.[[:digit:]]+$`. Indicating the end of the string
+#' prevents matching names that start as suspicious names but are not suspicious
+#' because they have non-digit characters appended, e.g. `X.2a`.
+#'
+#' @section Programming note 3: Could add argument 'allow_' and pass that to
+#' make.names(), to catch names with underscores?
+#'
+#' @seealso [paste_quoted()]
+#' @family collections of checks on type and length
+#'
+#' @examples
+#' all_names(x = names(c(a = 1, b = 2))) # TRUE
+#'
+#' all_names(x = names(c(a = 1, 2))) # FALSE: empty name.
+#' all_names(x = NULL) # FALSE: NULL
+#'
+#' all_names(x = c("a", "b", "a")) # FALSE: duplicated name
+#' all_names(x = c("a", "b", "a"), allow_dupl = TRUE) # TRUE
+#'
+#' all_names(x = "X.1") # FALSE: name created by read.csv()
+#' all_names(x = "X.1", allow_susp = TRUE) # TRUE
+#'
+#' all_names(x = "e.1") # FALSE: name modified by make.names()
+#' all_names(x = "e.1", allow_susp = TRUE) # TRUE
+#'
+#' @export
+all_names <- function(x, allow_dupl = FALSE, allow_susp = FALSE) {
+  stopifnot(is.null(dim(x)), is_logical(allow_dupl), is_logical(allow_susp))
+
+  warn_text <- character(0)
+  warn_text_dupl <- character(0)
+  suggest_make_names <- FALSE
+  chars_ok <- all_characters(x, allow_empty = FALSE, allow_zero = FALSE,
+                             allow_NA = FALSE)
+  if(!chars_ok && !is.null(x) && length(x) == 0L) {
+    warn_text <- c(warn_text, "zero-length values")
+    suggest_make_names <- TRUE
+  }
+
+  if(anyDuplicated(x) != 0L) {
+    bool_dupl <- duplicated(x)
+    if(!allow_dupl) {
+      warn_text_dupl <- paste0("duplicated names: '",
+                          paste0(unique(x[bool_dupl]), collapse = "', '"), "'")
+      suggest_make_names <- TRUE
+    }
+    x <- x[!bool_dupl]
+  }
+
+  # Notes:
+  # - make.names() replaces empty names ('""') with "X", so there is no need to
+  #   separately test for these.
+  # - Although make.names() replaces NAs in 'x' with "NA.", equality tests using
+  #   '==' or '!=' on the NAs in 'x' will still return NA (see 'Details' in
+  #   ?'=='). Therefore argument 'na.rm' in any() is set to TRUE to prevent
+  #   getting NA as condition and '| is.na(x)' is used to catch the NAs.
+  # - Argument 'unique' of make.names() is set to FALSE because duplicated names
+  #   have been catched above.
+  out_make_names <- make.names(x, unique = FALSE, allow_ = TRUE)
+  bool_invalid <- x != out_make_names
+  if(any(bool_invalid, na.rm = TRUE) || anyNA(x)) {
+    bool_zchar_x <- !nzchar(x)
+    bool_other_invalid <- is.na(x) | (bool_invalid & !bool_zchar_x)
+    invalid <- character(0)
+    if(any(bool_other_invalid)) {
+      invalid <- paste0(invalid, "'", paste0(x[bool_other_invalid],
+                                             collapse = "', '"), "'")
+    }
+    if(any(bool_zchar_x)) {
+      invalid <- paste0(c(invalid, "'\"\"' (i.e., an empty string)"),
+                        collapse = ", ")
+    }
+    warn_text <- c(warn_text, paste0("syntactically invalid values: ", invalid))
+    suggest_make_names <- TRUE
+    x <- x[!bool_invalid]
+  }
+
+  warn_text <- c(warn_text, warn_text_dupl)
+
+  if(!allow_susp) {
+    # See 'Programming note 2' for an explanation of the regular expressions.
+    bool_susp_v1 <- (x == "X" |
+                       grepl(pattern = "^X\\.[[:digit:]]+$", x = x, fixed = FALSE) |
+                       grepl(pattern = "^V[[:digit:]]+$", x = x, fixed = FALSE))
+    if(any(bool_susp_v1, na.rm = TRUE)) {
+      warn_text <- c(
+        warn_text,
+        paste0("names that might have been created by read.csv: '",
+               paste0(x[bool_susp_v1], collapse = "', '"), "'."))
+      x <- x[!bool_susp_v1]
+    }
+
+    bool_susp_v2 <- grepl(pattern = "\\.[[:digit:]]+$", x = x, fixed = FALSE)
+    if(any(bool_susp_v2, na.rm = TRUE)) {
+      warn_text <- c(
+        warn_text,
+        paste0("names that might have been modified by make.names(..., unique = TRUE): '",
+               paste0(x[bool_susp_v2], collapse = "', '"), "'."))
+    }
+  }
+
+  if(length(warn_text) > 0L) {
+    chars_ok <- FALSE
+    warn_text <- paste0("'x' contains ", paste0(warn_text, collapse = "; and "))
+  }
+
+  if(is.null(x)) {
+    chars_ok <- FALSE
+    warn_text_p1 <- paste0("'x' is NULL: did you use names(x) or colnames(x)",
+                           " on an object without\nnames or column names?")
+  } else {
+    warn_text_p1 <- character(0)
+  }
+
+  warn_text <- paste0(c(warn_text_p1, warn_text), collapse = " and ")
+  if(suggest_make_names) {
+    warn_text <- paste0(warn_text,
+                        ". Use 'x <- make.names(x, unique = TRUE)' to create",
+                        " unique, syntactically valid names!")
+  }
+
+  if(nchar(warn_text) > 0L) {
+    warning(x = warn_text)
+  }
+
+  chars_ok
+}
---
+++ inst/tinytest/test_all_names.R
@@ -0,0 +1,211 @@
+#### Create objects to use in tests ####
+false_true <- list(FALSE, TRUE)
+warn_syntax <- "syntactically invalid values: "
+warn_dupl <- "duplicated names: "
+warn_susp_v1 <- "names that might have been created by read.csv: '"
+warn_susp_v2 <- paste0("names that might have been modified by",
+                       " make.names\\(..., unique = TRUE): '")
+use_makenames <- ". Use 'x <- make.names"
+
+valid_nonsusp <- c("A", ".a", ".V1", ".V234", "VV1", "VV234", "X.", "X.1.",
+                   "X.234.", "V1.", "V234.", "X.1X", "X.234X", "V1V", "V234V",
+                   "X.a2", "X..", "X..X", "X.2X", "X.X", "X.X.X", "X.A", "A.X.A")
+valid_susp1 <- c("X", "X.1", "X.2", "X.234", "V1", "V234")
+valid_susp1_quoted <- paste(valid_susp1, collapse = "', '")
+valid_susp2 <- c("e.1", ".X.1", ".X.234", "XX.1", "XX.234", "X..1", "XX.2",
+                 "X.X.2", "X..2", "Xa.2", "A.X.2")
+valid_susp2_quoted <- paste(valid_susp2, collapse = "', '")
+# Although invalid could be suspicious, (e.g., "3X.1", "3X.234") invalid names
+# will not have been created by make.names()!
+invalid <- c("", ".3a", "for", "NA", "a/b", "a-b", "3a", "3X.1", "3X.234")
+empty_string_quoted <- "'\"\"' \\(i.e., an empty string)"
+invalid_quoted <- paste0("'", paste0(invalid[-1], collapse = "', '"),
+                         "', ", empty_string_quoted)
+
+
+#### Test the examples ####
+expect_true(all_names(x = names(c(a = 1, b = 2))))
+
+expect_warning(expect_false(
+  all_names(x = names(c(a = 1, 2)))),
+  pattern = paste0(warn_syntax, empty_string_quoted), strict = TRUE)
+expect_warning(expect_false(
+  all_names(x = NULL)), pattern = "'x' is NULL", strict = TRUE)
+
+expect_warning(expect_false(
+  all_names(x = c("a", "b", "a"))),
+  pattern = paste0(warn_dupl, "'a'"), strict = TRUE)
+expect_true(all_names(x = c("a", "b", "a"), allow_dupl = TRUE))
+
+expect_warning(expect_false(
+  all_names(x = "X.1", allow_susp = FALSE)),
+  pattern = paste0(warn_susp_v1, "X.1'"), strict = TRUE)
+expect_true(all_names(x = "X.1", allow_susp = TRUE))
+
+expect_warning(expect_false(
+  all_names(x = "e.1", allow_susp = FALSE)),
+  pattern = paste0(warn_susp_v2, "e.1'"), strict = TRUE)
+expect_true(all_names(x = "e.1", allow_susp = TRUE))
+
+
+#### Test section 'Details' ####
+expect_true(all_names("X.2a"))
+expect_equal(
+  make.names(names = rep(c("ab", "NA", "", NA_character_), each = 2L),
+             unique = TRUE),
+  c("ab", "ab.1", "NA.", "NA..1", "X", "X.1", "NA..2", "NA..3"))
+expect_equal(make.names(names = c(NA, NA), unique = TRUE),
+             c("NA.", "NA..1"))
+expect_equal(make.names(names = rep(c(NA_real_, 3), each = 2), unique = TRUE),
+             c("NA.", "NA..1", "X3", "X3.1"))
+
+
+#### Tests ####
+# allow_dupl <- FALSE
+# allow_susp <- FALSE
+for(allow_dupl in false_true) {
+  for(allow_susp in false_true) {
+    # zero-length values
+    expect_warning(expect_false(
+      all_names(x = NULL, allow_dupl = allow_dupl, allow_susp = allow_susp)),
+      pattern = "'x' is NULL", strict = TRUE)
+
+    expect_warning(expect_false(
+      all_names(x = character(0), allow_dupl = allow_dupl,
+                allow_susp = allow_susp)),
+      pattern = "zero-length values", strict = TRUE)
+
+    # Unique valid, not suspicious
+    expect_true(
+      all_names(x = valid_nonsusp, allow_dupl = allow_dupl,
+                allow_susp = allow_susp))
+
+    # Duplicated valid, not suspicious
+    if(allow_dupl) {
+      expect_true(
+        all_names(x = c(valid_nonsusp, valid_nonsusp[c(2, 5)]),
+                  allow_dupl = allow_dupl, allow_susp = allow_susp))
+    } else {
+      expect_warning(expect_false(
+        all_names(x = c(valid_nonsusp, valid_nonsusp[c(2, 5)]),
+                  allow_dupl = allow_dupl, allow_susp = allow_susp)),
+        pattern = paste0(warn_dupl, "'", paste0(valid_nonsusp[c(2, 5)],
+                                                collapse = "', '"), "'"),
+        strict = TRUE)
+    }
+
+    # Unique invalid, not suspicious
+    expect_warning(expect_false(
+      all_names(x = invalid, allow_dupl = allow_dupl, allow_susp = allow_susp)),
+      pattern = paste0(warn_syntax, invalid_quoted), strict = TRUE)
+
+    expect_warning(expect_false(
+      all_names(x = "", allow_dupl = allow_dupl, allow_susp = allow_susp)),
+      pattern = paste0(warn_syntax, empty_string_quoted, use_makenames),
+      strict = TRUE)
+
+    expect_warning(expect_false(
+      all_names(x = NA_character_, allow_dupl = allow_dupl,
+                allow_susp = allow_susp)),
+      pattern = paste0(warn_syntax, "'NA'", use_makenames), strict = TRUE)
+
+    expect_warning(expect_false(
+      all_names(x = NA, allow_dupl = allow_dupl, allow_susp = allow_susp)),
+      pattern = paste0(warn_syntax, "'NA'", use_makenames), strict = TRUE)
+
+    # Duplicated invalid, not suspicious
+    if(allow_dupl) {
+      expect_warning(expect_false(
+        all_names(x = c(invalid, invalid[2]), allow_dupl = allow_dupl,
+                  allow_susp = allow_susp)),
+        pattern = paste0(warn_syntax, invalid_quoted, use_makenames),
+        strict = TRUE)
+    } else {
+      expect_warning(expect_false(
+        all_names(x = c(invalid, invalid[2]), allow_dupl = allow_dupl,
+                  allow_susp = allow_susp)),
+        pattern = paste0(warn_syntax, invalid_quoted, "; and ", warn_dupl,
+                         "'", invalid[2], "'", use_makenames), strict = TRUE)
+    }
+
+    # Unique valid, suspicious v1
+    if(allow_susp) {
+      expect_true(
+        all_names(x = valid_susp1, allow_dupl = allow_dupl,
+                  allow_susp = allow_susp))
+    } else {
+      expect_warning(expect_false(
+        all_names(x = valid_susp1, allow_dupl = allow_dupl,
+                  allow_susp = allow_susp)),
+        pattern = paste0(warn_susp_v1, valid_susp1_quoted, "'"), strict = TRUE)
+    }
+
+    # Unique valid, suspicious v2
+    if(allow_susp) {
+      expect_true(
+        all_names(x = valid_susp2, allow_dupl = allow_dupl,
+                  allow_susp = allow_susp))
+    } else {
+      expect_warning(expect_false(
+        all_names(x = valid_susp2, allow_dupl = allow_dupl,
+                  allow_susp = allow_susp)),
+        pattern = paste0(warn_susp_v2, valid_susp2_quoted, "'"),
+        strict = TRUE)
+    }
+
+    # Mix
+    names_mix <- c(valid_nonsusp, valid_susp1, valid_susp2, invalid)
+
+    expect_warning(expect_false(
+      all_names(names_mix, allow_susp = TRUE)),
+      pattern = paste0(warn_syntax, invalid_quoted, use_makenames),
+      strict = TRUE)
+
+    expect_warning(expect_false(
+      all_names(names_mix, allow_susp = FALSE)),
+      pattern = paste0(warn_syntax, invalid_quoted, "; and ", warn_susp_v1,
+                       valid_susp1_quoted, "'.; and ", warn_susp_v2, # TO DO: remove extraneous dot.
+                       valid_susp2_quoted, "'.", use_makenames), # TO DO: remove extraneous dot.
+      strict = TRUE)
+  }
+}
+
+
+# Duplicated valid, suspicious v1
+expect_warning(expect_false(
+  all_names(x = c(valid_susp1, valid_susp1[c(2, 4)]), allow_dupl = FALSE,
+            allow_susp = TRUE)),
+  pattern = paste0(warn_dupl, "'", paste0(valid_susp1[c(2, 4)], collapse = "', '"),
+                   "'", use_makenames), strict = TRUE)
+
+expect_true(
+  all_names(x = c(valid_susp1, valid_susp1[c(2, 4)]), allow_dupl = TRUE,
+            allow_susp = TRUE))
+
+expect_warning(expect_false(
+  all_names(x = c(valid_susp1, valid_susp1[c(2, 4)]), allow_dupl = FALSE,
+            allow_susp = FALSE)),
+  pattern = paste0(warn_dupl, "'", paste0(valid_susp1[c(2, 4)], collapse = "', '"),
+                   "'; and ", warn_susp_v1, valid_susp1_quoted, "'"), strict = TRUE)
+
+expect_warning(expect_false(
+  all_names(x = c(valid_susp1, valid_susp1[c(2, 4)]), allow_dupl = TRUE,
+            allow_susp = FALSE)),
+  pattern = paste0(warn_susp_v1, valid_susp1_quoted, "'"), strict = TRUE)
+
+
+# Duplicated valid, suspicious v2
+# ... nog maken
+
+expect_error(all_names(),
+             pattern = "argument \"x\" is missing, with no default")
+expect_error(all_names(x = names(c(a = 1, b = 2)), allow_dupl = NA),
+             pattern = "is_logical\\(allow_dupl) is not TRUE")
+expect_error(all_names(x = names(c(a = 1, b = 2)), allow_susp = NA),
+             pattern = "is_logical\\(allow_susp) is not TRUE")
+
+
+#### Remove objects used in tests ####
+rm(allow_dupl, allow_susp, false_true, invalid, invalid_quoted, names_mix,
+   valid_nonsusp, valid_susp1, valid_susp1_quoted, valid_susp2,
+   valid_susp2_quoted, use_makenames, warn_syntax, warn_susp_v1, warn_susp_v2)
