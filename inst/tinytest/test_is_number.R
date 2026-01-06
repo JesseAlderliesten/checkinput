39b3a7484abc84a21d7ca7e10213e217053bab2e
JesseAlderliesten <jessealderliesten@hotmail.com>
2026-01-06 11:08:11 +0100

    Also test added examples. Test NA_complex_ as input.

--- inst/tinytest/test_is_number.R
+++ inst/tinytest/test_is_number.R
@@ -6,22 +6,23 @@ expect_identical(class(3L), "integer")
 
 
 #### Test the examples ####
-for(x in list(1, NA_real_)) {
+for(x in list(1, NA_real_, NaN, Inf)) {
   expect_true(is_number(x = x))
 }
 
-for(x in list(c(1, 2), "a", NA_character_)) {
+for(x in list(c(1, 2), "a", numeric(0), NA_character_)) {
   expect_false(is_number(x = x))
 }
 
 
 #### Tests ####
+# NaN is also considered a number.
 for(x in list(-Inf, -314, 0, 314, Inf, NaN)) {
   expect_true(is_number(x = x))
 }
 
 for(x in list(NULL, FALSE, TRUE, NA, numeric(0), c(NA_real_, NA_real_),
-              c(NaN, NaN), c(314, 314), character(0), NA_character_, "", "nco",
-              data.frame(a = 314), matrix(314), list(314))) {
+              c(NaN, NaN), c(314, 314), character(0), NA_character_, NA_complex_,
+              "", "nco", data.frame(a = 314), matrix(314), list(314))) {
   expect_false(is_number(x = x))
 }
