fa1f2e4cdc4f4458564c6139934545294deb49de
JesseAlderliesten <jessealderliesten@hotmail.com>
2026-01-09 17:26:30 +0100

    Added tests for negative and large positive numbers.

--- inst/tinytest/test_all_natural.R
+++ inst/tinytest/test_all_natural.R
@@ -5,6 +5,7 @@ expect_true(all_natural(x = c(1e-10, 3, 5), strict = FALSE))
 expect_false(all_natural(x = c(-1e-10, 3, 5), strict = FALSE))
 expect_false(all_natural(x = c(3, 5, Inf)))
 expect_false(all_natural(x = c(3, 5, Inf), strict = FALSE))
+expect_false(all_natural(x = "a"))
 
 x <- sqrt(2)^2
 expect_true(all_natural(x = x)) # TRUE
@@ -12,6 +13,8 @@ expect_false(x == 2)
 
 
 #### Tests ####
+expect_false(all_natural(x = -3L, strict = FALSE))
+
 expect_false(all_natural(x = 0, strict = TRUE))
 expect_true(all_natural(x = 0, strict = FALSE))
 expect_false(all_natural(x = 0L, strict = TRUE))
@@ -26,7 +29,13 @@ expect_true(all_natural(x = NA_integer_, allow_NA = TRUE))
 expect_false(all_natural(x = NA_real_, allow_NA = FALSE))
 expect_true(all_natural(x = NA_real_, allow_NA = TRUE))
 
-expect_true(all_natural(x = 1e20))
+# Testing a value that cannot be represented as integer by R (R uses 32-bit
+# integers, see 'Details' in help(`integer`))
+expect_true(all_natural(x = .Machine$integer.max + 10)) #
+
+expect_true(all_natural(x = c(3, 5 + 1e-10, 5.0, 1e4, 1.2e4)))
+expect_false(all_natural(x = TRUE))
+expect_false(all_natural(x = FALSE))
 
 for(allow_NA in c(FALSE, TRUE)) {
   expect_false(all_natural(x = NA_complex_, allow_NA = allow_NA))
