8bea6f8eb3834b1ef692c1c6eb783860e76029b9
JesseAlderliesten <jessealderliesten@hotmail.com>
2026-01-12 20:00:32 +0100

    Add a minimal version of paste_quoted as internal function.

---
+++ R/paste_quoted.R
@@ -0,0 +1,17 @@
+#' Internal function to concatenate a vector to a single string of quoted
+#' elements, representing `NULL` as `"'NULL'"` and other zero-length objects as
+#' `"'<class>(0)'"`, e.g., `"'logical(0)'"`. A fuller, exported version of this
+#' function is present in package `progutils`.
+#'
+#' @noRd
+paste_quoted <- function(x) {
+  if(is.null(x)) {
+    x <- "NULL"
+  }
+
+  if(length(x) == 0L) {
+    x <- paste0(class(x), "(0)")
+  }
+
+  paste0("'", paste(x, collapse = "', '"), "'")
+}
---
+++ inst/tinytest/test_paste_quoted.R
@@ -0,0 +1,50 @@
+#### Notes ####
+# Testing the INTERNAL function in 'checkinput'.
+# See the fuller, exported version of this function in package 'progutils'.
+
+
+#### Create objects to use in tests ####
+list_input <-  list("a", c("a", "b"), c("a", "b", "a"),
+                    c(3, 4), NULL, NA, NA_character_)
+list_output <- list("'a'", "'a', 'b'", "'a', 'b', 'a'",
+                    "'3', '4'", "'NULL'", "'NA'", "'NA'")
+list_input_zerolength <- list(NULL, character(0), numeric(0), logical(0), "")
+list_output_zerolength <- list("'NULL'", "'character(0)'", "'numeric(0)'",
+                               "'logical(0)'", "''")
+
+
+#### Tests ####
+for(index in seq_along(list_input)) {
+  expect_identical(paste_quoted(x = list_input[[index]]), list_output[[index]])
+}
+
+expect_error(
+  paste_quoted(3, 4),
+  pattern = "unused argument (4)", fixed = TRUE)
+
+expect_error(
+  paste_quoted(c(3, 4), 5:6),
+  pattern = "unused argument (5:6)", fixed = TRUE)
+
+expect_error(
+  paste_quoted(c(3, 4), 5:6, 7),
+  pattern = "unused arguments (5:6, 7)", fixed = TRUE)
+
+expect_error(
+  paste_quoted(c(3, 4), h = 5, 7),
+  pattern = "unused arguments (h = 5, 7)", fixed = TRUE)
+
+expect_identical(paste_quoted(c(3, 4)), "'3', '4'")
+expect_identical(paste_quoted(c(a = 3, b = 4)), "'3', '4'")
+expect_identical(paste_quoted(NULL), "'NULL'")
+
+for(index_NULL in seq_along(list_input_zerolength)) {
+  expect_silent(expect_identical(
+    paste_quoted(list_input_zerolength[[index_NULL]]),
+    list_output_zerolength[[index_NULL]]))
+}
+
+
+#### Remove objects used in tests ####
+rm(index, index_NULL, list_input, list_input_zerolength, list_output,
+   list_output_zerolength)
