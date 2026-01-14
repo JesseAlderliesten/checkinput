dc72d6b51b5c5ca87ce4ef4f833d31bf3fa94eab
JesseAlderliesten <jessealderliesten@hotmail.com>
2026-01-14 19:36:35 +0100

    `is_number()` and derived functions gained argument `allow_zero`. Solve issue #3.

--- inst/tinytest/test_all_nonnegative.R
+++ inst/tinytest/test_all_nonnegative.R
@@ -1,16 +1,28 @@
+#### Create objects to use in tests ####
+false_true <- list(FALSE, TRUE)
+
+
 #### Test the examples ####
 expect_silent(expect_true(all_nonnegative(x = c(3, 0))))
+expect_silent(expect_true(all_nonnegative(numeric(0), allow_zero = TRUE)))
 
 
 #### Tests ####
-for(x in list(c(0, 0), c(314, 314), c(Inf, Inf))) {
-  expect_silent(expect_true(all_nonnegative(x = x)))
+for(allow_zero in false_true) {
+  for(x in list(c(0, 0), c(314, 314), c(Inf, Inf))) {
+    expect_silent(expect_true(all_nonnegative(x = x, allow_zero = allow_zero)))
+  }
 }
 
-for(x in list(NULL, FALSE, TRUE, NA, character(0), numeric(0), "",
-              NA_character_, NA_complex_, "nco", -Inf -314, c(314, -314),
-              data.frame(a = 314), as.matrix(data.frame(a = 314)), list())) {
-  expect_silent(expect_false(all_nonnegative(x = x)))
+expect_silent(expect_true(is_number(x = numeric(0), allow_zero = TRUE)))
+expect_silent(expect_false(is_number(x = numeric(0), allow_zero = FALSE)))
+
+for(allow_zero in false_true) {
+  for(x in list(NULL, FALSE, TRUE, NA, character(0), "",
+                NA_character_, NA_complex_, "nco", -Inf -314, c(314, -314),
+                data.frame(a = 314), as.matrix(data.frame(a = 314)), list())) {
+    expect_silent(expect_false(all_nonnegative(x = x)))
+  }
 }
 
 for(x in list(NaN, NA_real_)) {
@@ -19,4 +31,4 @@ for(x in list(NaN, NA_real_)) {
 
 
 #### Remove objects used in tests ####
-rm(x)
+rm(allow_zero, false_true, x)
--- inst/tinytest/test_is_nonnegative.R
+++ inst/tinytest/test_is_nonnegative.R
@@ -1,16 +1,27 @@
+#### Create objects to use in tests ####
+false_true <- list(FALSE, TRUE)
+
+
 #### Test the examples ####
 expect_true(is_nonnegative(x = 3))
 
 
 #### Tests ####
-for(x in list(0, 314, Inf)) {
-  expect_silent(expect_true(is_nonnegative(x = x)))
+for(allow_zero in false_true) {
+  for(x in list(0, 314, Inf)) {
+    expect_silent(expect_true(is_nonnegative(x = x, allow_zero = allow_zero)))
+  }
 }
 
-for(x in list(NULL, FALSE, TRUE, NA, numeric(0), c(NaN, NaN), c(NaN, NA_real_),
-              c(NA_real_, NA_real_), character(0), NA_character_, "", "nco",
-              -Inf, -314, c(0, 0), c(314, 314), c(Inf, Inf))) {
-  expect_silent(expect_false(is_nonnegative(x = x)))
+expect_silent(expect_true(is_number(x = numeric(0), allow_zero = TRUE)))
+expect_silent(expect_false(is_number(x = numeric(0), allow_zero = FALSE)))
+
+for(allow_zero in false_true) {
+  for(x in list(NULL, FALSE, TRUE, NA, c(NaN, NaN), c(NaN, NA_real_),
+                c(NA_real_, NA_real_), character(0), NA_character_, "", "nco",
+                -Inf, -314, c(0, 0), c(314, 314), c(Inf, Inf))) {
+    expect_silent(expect_false(is_nonnegative(x = x)))
+  }
 }
 
 for(x in list(NaN, NA_real_)) {
@@ -19,4 +30,4 @@ for(x in list(NaN, NA_real_)) {
 
 
 #### Remove objects used in tests ####
-rm(x)
+rm(allow_zero, false_true, x)
