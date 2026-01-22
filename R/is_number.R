9b646f3ae340e379e1e8ee4eb5b43792e0c660dd
JesseAlderliesten <jessealderliesten@hotmail.com>
2026-01-22 13:32:44 +0100

    is_number() and derived functions gained arguments allow_zero, allow_NA and allow_NaN.

--- R/is_number.R
+++ R/is_number.R
@@ -4,19 +4,30 @@
 #' correct sign.
 #'
 #' @inheritParams is_logical
+#' @param allow_NA `TRUE` or `FALSE`: allow numeric [NA]s (i.e., `NA_integer_`
+#' and `NA_real_`)?
+#' @param allow_NaN `TRUE` or `FALSE`: allow [NaN]s?
 #'
 #' @details
-#' The correct length of `x` is larger than zero for `all_numbers()`
-#' and one for `is_number()`.
+#' The correct length of `x` is larger than zero for `all_...()` and one for
+#' `is_...()`, unless `allow_zero` is `TRUE`: then numeric-type zero-length `x`
+#' is also allowed for both types of functions.
 #'
 #' `all_nonnegative()` and `is_nonnegative()` return `TRUE` for `0`, whereas
 #' `is_positive()` returns `FALSE` for `0`.
 #'
-#' `all_numbers()` and `is_number()` return `TRUE` for `Inf`, `NA_real_`,
-#' `NA_integer_`, and `NaN` because their `mode` is `numeric`.
+#' `all_...()` and `is_...()` return `TRUE` for `-Inf` and `Inf` if it has the
+#' correct sign.
 #'
-#' @returns `TRUE` or `FALSE` indicating if `x` is a numeric vector of the
-#' correct length with numbers of the correct sign.
+#' `all_...()` and `is_...()` return `FALSE` for `NA_complex_`, even if
+#' `allow_NA` is `TRUE`, because its mode is `complex` instead of `numeric`.
+#'
+#' `NaN` has mode `numeric`, despite its meaning 'not a number'.
+#'
+#' @returns
+#' `TRUE` or `FALSE` indicating if `x` is a numeric vector of the correct length
+#' with numbers of the correct sign that adheres to the limitations set by the
+#' other arguments.
 #'
 #' @section Programming note:
 #' [is.numeric()] tests the [mode()] of `x`, which is `numeric` for
@@ -25,13 +36,8 @@
 #' for floating-point numbers but `integer` for integers (see the `Note on names`
 #' in [is.numeric()]).
 #'
-#' Adding arguments `allow_NA` and `allow_NaN` to optionally allow `NA_real_`,
-#' `NA_integer_`, or `NaN` (which all have `mode` `numeric`) would solve the
-#' contradiction that using `is_number()` currently returns `TRUE` for these
-#' values but using them in conditional statements leads to `logical(0)` and
-#' thus to an error. However, implementing those arguments is complicated
-#' because `is.na(x)` returns `TRUE` for `NaN` and `is.na(x)` and `is.nan(x)`
-#' return `logical(0)` for zero-length `x`.
+#' The functions duplicate code instead of calling `is_number()` or
+#' `all_numbers()`, to prevent performing checks twice.
 #'
 #' @family
 #' collections of checks on type and length
@@ -41,54 +47,101 @@
 #' [vignette about type coercion](../doc/type_coercion.html).
 #'
 #' @examples
-#' is_number(1) # TRUE
-#' is_number(c(1, 2)) # FALSE: incorrect length
-#' all_numbers(c(1, 2)) # TRUE
-#' is_number("a") # FALSE: incorrect type
-#' is_number(numeric(0)) # FALSE: incorrect length
-#' is_number(numeric(0), allow_zero = TRUE) # TRUE
-#' is_number(NA_real_) # TRUE: 'allow_NA' has not been implemented
-#' is_number(NA_character_) # FALSE: incorrect type
-#' is_number(NaN) # TRUE (!)
-#' is_number(Inf) # TRUE
-#' is_nonnegative(3) # TRUE
-#' is_nonnegative(0) # TRUE
-#' all_nonnegative(c(3, 0)) # TRUE
-#' all_nonnegative(numeric(0), allow_zero = TRUE) # TRUE
-#' is_positive(3) # TRUE
-#' is_positive(0) # FALSE
+#' is_number(x = 1) # TRUE
+#' is_number(x = 3.14) # TRUE
+#' is_number(x = c(1, 2)) # FALSE: incorrect length
+#' all_numbers(x = c(1, 2)) # TRUE
+#' is_number(x = "a") # FALSE: incorrect type
+#' is_number(x = numeric(0)) # FALSE: incorrect length
+#' is_number(x = numeric(0), allow_zero = TRUE) # TRUE
+#' is_number(x = NA_real_) # FALSE
+#' is_number(x = NA_real_, allow_NA = TRUE) # TRUE
+#' is_number(x = NA_character_, allow_NA = TRUE) # FALSE: incorrect type
+#' is_number(x = NaN, allow_NA = TRUE) # FALSE, need allow_NaN = TRUE to allow NaN
+#' is_number(x = NaN, allow_NaN = TRUE) # TRUE
+#' is_number(x = Inf) # TRUE
+#' is_nonnegative(x = 3) # TRUE
+#' is_nonnegative(x = 0) # TRUE
+#' all_nonnegative(x = c(3, 0)) # TRUE
+#' all_nonnegative(x = numeric(0), allow_zero = TRUE) # TRUE
+#' is_positive(x = 3) # TRUE
+#' is_positive(x = 0) # FALSE
 #'
 #' @export
-is_number <- function(x, allow_zero = FALSE) {
+is_number <- function(x, allow_zero = FALSE, allow_NA = FALSE,
+                      allow_NaN = FALSE) {
   # is.null(dim(x)) is needed to return `FALSE` for matrices with a single value.
   is.numeric(x) && is.atomic(x) && is.null(dim(x)) &&
-    (length(x) == 1L || (allow_zero && length(x) == 0L))
+    ((allow_zero && length(x) == 0L) ||
+       (length(x) == 1L &&
+          (allow_NA || !any(is.na(x) & !is.nan(x))) &&
+          (allow_NaN || !any(is.nan(x)))))
 }
 
 #' @rdname is_number
 #' @export
-all_numbers <- function(x, allow_zero = FALSE) {
+all_numbers <- function(x, allow_zero = FALSE, allow_NA = FALSE,
+                        allow_NaN = FALSE) {
+  # is.null(dim(x)) is needed to return `FALSE` for matrices with a single value.
   is.numeric(x) && is.atomic(x) && is.null(dim(x)) &&
-    (allow_zero || length(x) > 0L)
+    ((allow_zero && length(x) == 0L) ||
+       (length(x) >= 1L &&
+          (allow_NA || !any(is.na(x) & !is.nan(x))) &&
+          (allow_NaN || !any(is.nan(x)))))
 }
 
 #' @rdname is_number
 #' @export
-is_nonnegative <- function(x, allow_zero = FALSE) {
-  is_number(x, allow_zero = allow_zero) &&
-    ((allow_zero && length(x) == 0L) || x >= 0)
+is_nonnegative <- function(x, allow_zero = FALSE, allow_NA = FALSE,
+                           allow_NaN = FALSE) {
+  length_x <- length(x)
+  # is.null(dim(x)) is needed to return `FALSE` for matrices with a single value.
+  ok_p1 <- is.numeric(x) && is.atomic(x) && is.null(dim(x)) &&
+    length_x <= 1L && (allow_zero || length_x == 1L)
+
+  if(ok_p1) {
+    if(length_x == 1L) {
+      is_NA_x <- is.na(x)
+      is_NaN_x <- is.nan(x)
+      ok_p2 <- allow_NaN || !is_NaN_x
+      ok_p3 <- allow_NA || !is_NA_x || is_NaN_x
+      ok_p2 && ok_p3 && (is_NA_x || x >= 0)
+    } else {
+      TRUE
+    }
+  } else {
+    FALSE
+  }
 }
 
 #' @rdname is_number
 #' @export
-all_nonnegative <- function(x, allow_zero = FALSE) {
-  all_numbers(x, allow_zero = allow_zero) &&
-    ((allow_zero && length(x) == 0L) || all(x >= 0))
+all_nonnegative <- function(x, allow_zero = FALSE, allow_NA = FALSE,
+                            allow_NaN = FALSE) {
+  # is.null(dim(x)) is needed to return `FALSE` for matrices with a single value.
+  is.numeric(x) && is.atomic(x) && is.null(dim(x)) &&
+    ((allow_zero && length(x) == 0L) ||
+       (length(x) > 0L &&
+          (allow_NA || !any(is.na(x) & !is.nan(x))) &&
+          (allow_NaN || !any(is.nan(x))) &&
+          all(x >= 0, na.rm = TRUE)))
 }
 
 #' @rdname is_number
 #' @export
-is_positive <- function(x, allow_zero = FALSE) {
-  is_number(x, allow_zero = allow_zero) &&
-    ((allow_zero && length(x) == 0L) || x > 0)
+is_positive <- function(x, allow_zero = FALSE, allow_NA = FALSE,
+                        allow_NaN = FALSE) {
+  length_x <- length(x)
+  # is.null(dim(x)) is needed to return `FALSE` for matrices with a single value.
+  ok_p1 <- is.numeric(x) && is.atomic(x) && is.null(dim(x)) &&
+    length_x <= 1L && (allow_zero || length_x == 1L)
+
+  if(ok_p1) {
+    is_NA_x <- is.na(x)
+    is_NaN_x <- is.nan(x)
+    length_x == 0L || ((allow_NaN || !is_NaN_x) && (allow_NA || !is_NA_x || is_NaN_x) &&
+                         all(x > 0, na.rm = TRUE))
+  } else {
+    FALSE
+  }
 }
